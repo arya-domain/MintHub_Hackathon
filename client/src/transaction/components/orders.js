94a5d436ac199bfa21ac0750bb4f4805fb269734
arya-domain <aryandas156@gmail.com>
2023-04-17 02:19:16 +0530

    ./

--- client/src/transaction/components/orders.js
+++ client/src/transaction/components/orders.js
@@ -6,7 +6,7 @@ export const Orders = () => {
 
   useEffect(() => {
     const authToken = localStorage.getItem("authtoken");
-  
+
     axios.post('http://localhost:8080/api/auth/getuser', {}, {
       headers: {
         'Content-Type': 'application/json',
@@ -28,31 +28,56 @@ export const Orders = () => {
   }
 
   return (
-    <div className="bg-gray-800 text-white min-h-screen py-8 px-4 sm:px-6 lg:px-8">
-      <div><h1 className="text-4xl">C2C Transaction</h1>
-      {orders.length > 0 ? (
-        orders.map(order => (
-          <div key={order._id} className="bg-gray-700 p-4 rounded-lg shadow-md mb-4">
-          
-            <p className="text-xl font-medium mb-2 cursor-pointer" onClick={() => toggleExpanded(order._id)}>Order ID: {order._id}</p>
-            {expandedOrderId === order._id && (
-              <div className="mt-2">
-                <p className="text-lg mb-2">Sending Amount: {order.sending_amt}</p>
-                <p className="text-lg mb-2">Charge: {order.charge}</p>
-                <p className="text-lg mb-2">Total: {order.total}</p>
-                <p className="text-lg mb-2">Account No.: {order.account_no}</p>
-                <p className="text-lg mb-2">Bank Name: {order.bankname}</p>
-                <p className="text-lg mb-2">IFSC: {order.ifsc}</p>
-                <p className="text-lg mb-2">Address: {order.address}</p>
-              </div>
-            )}
-            <hr className="my-2 border-gray-600"/> {/* Add a horizontal line to separate orders */}
-          </div>
-        ))
-      ) : (
-        <p>No orders found.</p>
-      )}
-    </div>
+    <div className="text-white text-xl text-center min-h-screen justify-content-center align-items-center bg-gradient-to-b from-black via-purple-700 to-black pt-4">
+      <div className=""><h1 className="text-4xl font-extrabold">C2C Transactions</h1><br></br>
+        {orders.length > 0 ? (
+          orders.map(order => (
+
+            <div key={order._id} className="card bg-black border-white w-1/2 mx-auto">
+              <p className="text-xl font-medium mb-2 cursor-pointer" onClick={() => toggleExpanded(order._id)}>Order ID: {order._id}</p>
+              {expandedOrderId === order._id && (
+
+                <table className="mt-2">
+                  <tbody>
+                    <tr>
+                      <td><p className="text-lg mb-2 text-left px-5">Sending Amount:</p></td>
+                      <td><p className="text-lg mb-2 text-left px-5">{order.sending_amt}</p></td>
+                    </tr>
+                    <tr>
+                      <td><p className="text-lg mb-2 text-left px-5">Charge:</p></td>
+                      <td><p className="text-lg mb-2 text-left px-5">{order.charge}</p></td>
+                    </tr>
+                    <tr>
+                      <td><p className="text-lg mb-2 text-left px-5">Total:</p></td>
+                      <td><p className="text-lg mb-2 text-left px-5">{order.total}</p></td>
+                    </tr>
+                    <tr>
+                      <td><p className="text-lg mb-2 text-left px-5">Account No.:</p></td>
+                      <td><p className="text-lg mb-2 text-left px-5">{order.account_no}</p></td>
+                    </tr>
+                    <tr>
+                      <td><p className="text-lg mb-2 text-left px-5">Bank Name:</p></td>
+                      <td><p className="text-lg mb-2 text-left px-5">{order.bankname}</p></td>
+                    </tr>
+                    <tr>
+                      <td><p className="text-lg mb-2 text-left px-5">IFSC:</p></td>
+                      <td><p className="text-lg mb-2 text-left px-5">{order.ifsc}</p></td>
+                    </tr>
+                    <tr>
+                      <td><p className="text-lg mb-2 text-left px-5">Address:</p></td>
+                      <td><p className="text-lg mb-2 text-left px-5">{order.address}</p></td>
+                    </tr>
+                  </tbody>
+                </table>
+
+              )}
+              <hr className="my-2 border-l-white" /> {/* Add a horizontal line to separate orders */}
+            </div>
+          ))
+        ) : (
+          <p>No orders found.</p>
+        )}
+      </div>
     </div>
   );
 }
--- j.js
+++ j.js
@@ -1,21 +1,78 @@
-import { Row,Col } from "react-bootstrap"
+import React, { useState, useEffect } from 'react';
+import Web3 from 'web3';
 
-export const Search = () => {
-    const mj = localStorage.getItem("marketjson")
+function Transfer() {
+  const [web3, setWeb3] = useState(null);
+  const [recipientAddress, setRecipientAddress] = useState('0xe845f8786533E0d380E6b5e3f3BA56fcC599D9Bd');
 
-    return(
-        <div>
-            <Row className="px-2 rounded-md items-left border-gray-50 -scale-x-100">
-                <Col>
-                  <input type="search" id="searchBar" className="w-3/5 text-left outline-none form-control active:outline-none focus:outline-none" placeholder="Search" aria-label="Search" aria-describedby="search-addon" /></Col>
-                <Col className="mx-[2%]">
-                  <button className="relative px-3 py-2 text-sm duration-200 ease-in border-2 border-white rounded-md hover:bg-white hover:text-black right-1/2 " >
-                    Find
-                  </button>
-                  <div></div>
-                </Col>
 
-              </Row>
-        </div>
-    )
+  useEffect(() => {
+    async function loadWeb3() {
+      // Check if Web3 has been injected by the browser (Metamask)
+      if (window.ethereum) {
+        // Create a new Web3 instance using the injected provider
+        const web3Instance = new Web3(window.ethereum);
+        setWeb3(web3Instance);
+
+        try {
+          // Request account access if needed
+          await window.ethereum.enable();
+
+          // Get the current account from Metamask
+          const accounts = await web3Instance.eth.getAccounts();
+          setCurrentAccount(accounts[0]);
+        } catch (error) {
+          console.error('Failed to connect to Metamask:', error);
+        }
+      } else {
+        console.error('Metamask not detected');
+      }
+    }
+
+    loadWeb3();
+  }, []);
+
+  async function handleTransfer() {
+    // Convert the amount to transfer from Ether to Wei
+    const amountToSend = web3.utils.toWei(receiver.symval, 'ether');
+
+    // Create the transaction object
+    const txObject = {
+      from: currentAccount,
+      to: recipientAddress,
+      value: amountToSend
+    };
+
+    try {
+      // Sign and send the transaction
+      const txHash = await web3.eth.sendTransaction(txObject);
+      console.log('Transaction hash:', txHash);
+    } catch (error) {
+      console.error('Transaction error:', error);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Transfer Ether</h1>
+      {currentAccount ? (
+        <>
+          <p>Current account: {currentAccount}</p>
+          <label>
+            Recipient address:
+            <input type="text" value={recipientAddress} onChange={(e) => setRecipientAddress(e.target.value)} />
+          </label>
+          <br />
+          <label>
+            Amount (ETH):
+            <input type="number" step="0.01" value={receiver.symval} onChange={(e) => setAmount(e.target.value)} />
+          </label>
+          <br />
+          <button onClick={handleTransfer}>Transfer</button>
+        </>
+      ) : (
+        <p>Connect your Metamask wallet to transfer Ether</p>
+      )}
+    </div>
+  );
 }
